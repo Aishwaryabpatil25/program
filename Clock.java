51b0fd7bc230ca256fe1ccb3bad8e52eaf8ef03f
Aishwaryabpatil25 <aishwaryabpatil.xworkz@gmail.com>
2023-12-13 21:35:56 +0530

    Arrays in java

---
+++ Clock.java
@@ -0,0 +1,25 @@
+public class Clock
+{
+public static void main(String args[])
+{
+   int [] numbers ={10,20,30,40,50,60,70,80,90,100,120,130,140,5,15,25,35,45,55,65};
+   float[] percentage ={80.2f,55.3f,45.5f,60.5f,89.2f,70.3f,75.1f,30.0f,45.5f,85.5f,65.2f,35.56f,28.23f,31.23f,95.5f,25.67f,88.35f,83.21f,82.2f,94.11f,72.56f};
+   double[] bal ={45.0000d,65.3000d,15.000d,35.3000d,92.3000d,65.3000d,11.0000d,82.3000d,65.3000d,65.3000d,22.2000d,80.0000d,26.3000d,25.3000d,66.3000d,88.3000d,77.3000d,33.3000d,45.3000d,55.0000d};
+   short[] rollNum={(short)101,(short)102,(short)103,(short)104,(short)105,(short)106,(short)107,(short)108,(short)109,(short)110,(short)111,(short)112,(short)113,(short)114,(short)115,(short)116,(short)117,(short)118,(short)119,(short)120};
+   char[] consonants ={'b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y'};
+   byte[] age ={(byte)10,(byte)20,(byte)21,(byte)23,(byte)25,(byte)35,(byte)65,(byte)43,(byte)24,(byte)65,(byte)75,(byte)80,(byte)90,(byte)92,(byte)45,(byte)64,(byte)37,(byte)28,(byte)32,(byte)72};
+   long[] phoneNum ={8660745259l,9980721356l,6680721356l,7760721356l,8660721356l,8660721356l,8095653076l,8660721356l,8660721356l,8660721356l,8660721356l,8660721356l,8660721356l,8660721356l,8660721356l,8660721356l,8660721356l,8660721356l,8660721356l,8660721356l};
+   boolean[] pass={true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,false,true,false,true};
+     for(int i=0;i<20;i++){
+        System.out.println(numbers[i]);
+        System.out.println(percentage[i]);
+        System.out.println(bal[i]);
+        System.out.println(rollNum[i]);
+        System.out.println(consonants[i]);
+        System.out.println(age [i]);
+        System.out.println(phoneNum[i]);
+        System.out.println(pass[i]);
+    
+}
+}
+}
