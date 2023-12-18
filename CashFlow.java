2fe00b96b3057c31c28b609a37f429e4cc91c208
Aishwaryabpatil25 <aishwaryabpatil.xworkz@gmail.com>
2023-12-18 08:32:05 +0530

    Type Casting in java

---
+++ CashFlow.java
@@ -0,0 +1,49 @@
+public class CashFlow{
+	
+	public static void main(String args[])
+	{
+		
+		byte number=25;
+		short value=number;
+		System.out.println(value);
+	
+		short number1=300;
+		int value1=number1;
+		System.out.println(value1);
+
+		int number2=30000;
+		long value2=number2;
+		System.out.println(value2);
+
+		long number3=1212311234l;
+		int value3=(int)number3;
+		System.out.println(value3);
+
+		int number4=12453;
+		short value4=(short)number4;
+		System.out.println(value4);
+
+		short number5=65;
+		byte value5=(byte)number5;
+		System.out.println(value5);
+
+		int number6=82;
+		char value6=(char)number6;
+		System.out.println(value6);
+
+		char char1='a';
+		int value7=char1;
+		System.out.println(value7);
+
+		float number8=43.543f;
+		double value8=number8;
+		System.out.println(value8);
+		
+
+		double number9=65.23146d;
+		float value9=(float)number9;
+		System.out.println(value9);
+	
+
+	}
+}
