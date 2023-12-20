d6a186c84391eb039d6434c1b13039c086cb4e9c
Aishwaryabpatil25 <aishwaryabpatil.xworkz@gmail.com>
2023-12-20 18:40:49 +0530

    printing using methods

---
+++ Book.java
@@ -0,0 +1,45 @@
+public class Book
+{
+public static void main(String [] args)
+{
+   int [] marks ={300,400,500,600,700,800,900,200};
+   for(int i=0;i<marks.length;i++)
+    {
+      System.out.println(marks[i]);
+}
+  char [] consonants ={'b','c','d','f','g','h','j','k'};
+  for(char i=0;i<consonants.length;i++)
+   {
+     System.out.println(consonants[i]);
+}
+  float [] temp ={30.5f,42.2f,50.3f,60.4f,23.3f,34.6f,65.1f,70.2f};
+   for(float index=0;index<temp.length;index++)
+    {
+      System.out.println(temp[index]);    
+}
+   byte [] age ={30,40,50,60,70,80,35,24};
+    for(byte i=0;i<age.length;i++)
+    {
+       System.out.println(age[i]);    
+}
+  boolean [] result ={true,false,false,true,true,false,false,true};
+   for(boolean index=0;index<result.length;index++)
+    {
+      System.out.println(result[index]) ; 
+} 
+   short [] emp ={10,20,30,40,50,60,70,80}; 
+    for(short i=0;i<emp.length;i++)
+    {
+     System.out.println(emp[i]) ; 
+} 
+ 
+   int [] studentId ={1,2,3,4,5,6,7,8};
+    for(int i=studentId.length;i<studentId.length-1;i--)
+    {
+     System.out.println(studentId[i]);
+}
+
+
+}
+}
+  
---
+++ Books.java
@@ -0,0 +1,75 @@
+public class Books
+{
+   String bookName;
+   int noOfPages;
+   String  author; 
+   double  price;
+   int  publishedYear;
+   String publisher;
+   String  type;
+
+    public Books(String bookName,int noOfPages,String  author,double  price,int  publishedYear,String publisher,String  type)
+       {
+        this.bookName=bookName;
+        this.noOfPages=noOfPages;
+        this.author=author;
+        this.price=price;
+        this.publishedYear=publishedYear;
+        this.publisher=publisher;
+        this.type=type;
+       }
+      
+
+   public void printBookDetails()
+      {
+         System.out.println("Book :" +bookName);
+         System.out.println("Pages:" +noOfPages);
+         System.out.println("Author:" +author);
+         System.out.println("Price :" +price);
+         System.out.println("Published Year :" +publishedYear);
+         System.out.println("Publisher:" +publisher);
+         System.out.println("Type:" +type);
+        }
+   public static void main(String args[])
+      {
+         Books book1 = new Books("C-Programming",400,"Dennis Ritchie", 30000, 2020, "Prentice Hall", "Programming");
+
+        
+         Books book2 = new Books("Java Programming",500,"John Doe", 22000, 2020, "Tech Books Inc", "Programming");
+         
+         Books book3 = new Books("R-Programming",600,"Kurt Hornik",40000,1997," Allaire","R program");
+         
+         Books book4 = new Books("Python",560,"Guido van Rossum",10000,1991,"  Python Software Foundation","python");
+
+         Books book5 = new Books("Data Structure",700,"Anuradha A. Puntambekar",3200,2020," Technical Publications","DS");
+
+         Books book6 = new Books("Data Science",600,"Jane Smith", 20000, 2021, "DataTech Publishing", "Data Science");
+
+         Books book7  = new Books("Data analyst",300,"Bharti Motwani",25000,1996,"  BCS  ","  Data analyst ");
+     
+
+         book1.printBookDetails();
+
+           System.out.println("------------------------------------------------------------------------");
+         book2.printBookDetails();
+           System.out.println("------------------------------------------------------------------------");
+
+         book3.printBookDetails();
+           System.out.println("------------------------------------------------------------------------");
+
+         book4.printBookDetails();
+             System.out.println("------------------------------------------------------------------------");
+
+         book5.printBookDetails();
+              System.out.println("------------------------------------------------------------------------");
+
+         book6.printBookDetails();
+              System.out.println("------------------------------------------------------------------------");
+
+         book7.printBookDetails();
+    }
+ }
+        
+
+
+
