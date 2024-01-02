371cd64e8e926d65697f513311a0602891ffa161
Aishwaryabpatil25 <aishwaryabpatil.xworkz@gmail.com>
2024-01-02 19:21:31 +0530

    Pattern Matching and Methods

---
+++ Examples/Simple.java
@@ -0,0 +1,22 @@
+public class Simple
+{
+   public static void main(String args[])
+     {
+       double val1=Math.min(5,10);
+       System.out.println(val1);
+   
+       double val2=Math.ceil(10.45);
+       System.out.println(val2);
+   
+       double val3=Math.floor(10.45);
+       System.out.println(val3);
+      
+       double val4=Math.subtractExact(4,2);
+       System.out.println(val4);
+
+       double val5=Math.log(38.9);
+       System.out.println(val5);
+      
+ }
+}
+      
