e64dc7dcd8749bf3319f453e1fbf7b40f2e5a888
Aishwaryabpatil25 <aishwaryabpatil.xworkz@gmail.com>
2024-01-03 18:39:49 +0530

    String and Pattern

---
+++ Examples/Institute.java
@@ -0,0 +1,125 @@
+public class Institute
+  {
+   public static void main(String args[])
+     {
+       String newsChannel = "Republic News";
+         System.out.println(newsChannel);
+            newsChannel = "TV9";
+        System.out.println(newsChannel);
+        System.out.println(newsChannel == newsChannel);
+        System.out.println("-----------------------------");
+
+      
+       String collegeName = "GIT";
+         System.out.println(collegeName);
+           collegeName = "Prism";
+        System.out.println(collegeName);  
+        System.out.println("-----------------------------");
+ 
+    
+       String subject = "Data Structure";
+          System.out.println(subject);
+           subject = "unix";
+          System.out.println(subject);
+          System.out.println("-----------------------------");
+       
+       String greeting = "Good evening";
+         System.out.println(greeting);
+          greeting = "good night";
+         System.out.println(greeting);
+         System.out.println("-----------------------------");
+      
+       String speakinglanguage = "Kannada";
+         System.out.println(speakinglanguage);
+          System.out.println(speakinglanguage == speakinglanguage);
+         System.out.println("-----------------------------"); 
+      
+       String nickName = "Aishu";
+          System.out.println(nickName);
+            nickName = "deepu";
+          System.out.println(nickName);
+          System.out.println("-----------------------------");
+       
+       String novel ="Think like a monk";
+          System.out.println(novel);
+          System.out.println("-----------------------------");
+       
+       String onlineShopName = "Amazon";
+         System.out.println(onlineShopName);  
+         System.out.println("-----------------------------");
+       
+       String authorOfLinux = "Richard Petersen";
+          System.out.println(authorOfLinux);
+          System.out.println("-----------------------------");
+       
+       String bankName = "SBI";
+          System.out.println(bankName);
+           System.out.println("-----------------------------");
+       
+       String newsPaperName = "Deccan Herald";
+        System.out.println(newsPaperName);
+
+ System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+     
+     String address = new String("Patil");
+     System.out.println(address);
+       address = "Reddy";//re-assigning
+     System.out.println(address);
+     System.out.println("-----------------------------");      
+    
+     String programming = new String("C-Programming");
+     System.out.println(programming);
+         programming = "R-Programming";
+     System.out.println(programming);
+      System.out.println("-----------------------------");
+    
+     String houseType = new String("Cottage");
+     System.out.println(houseType);
+         houseType = "FarmHouse";
+     System.out.println(houseType);
+      System.out.println("-----------------------------");
+    
+     String state = new String("Karnataka");
+     System.out.println(state);
+        state = "Goa";
+     System.out.println(state);
+     System.out.println("-----------------------------");
+    
+     String countrie = new String ("Algeria");
+     System.out.println(countrie);
+       countrie = "India";
+     System.out.println(countrie);
+     System.out.println("-----------------------------");
+     
+     String  value1 = new String("Infynow");
+     System.out.println(value1);
+     System.out.println("-----------------------------");
+     
+     String  value = new String ("Aish");
+     System.out.println(value);
+     System.out.println(value == nickName);
+     System.out.println("-----------------------------");
+    
+     String  festival = new String("Diwali");
+     System.out.println(festival);
+      System.out.println("-----------------------------");
+     
+     String  value4 = new String ("Data Structure");
+     System.out.println(value4);
+     System.out.println(value4 == subject);
+     System.out.println("-----------------------------");
+     
+     String  value5 = new String ("FastFood");
+     System.out.println(value5);
+     System.out.println(value5 == value5);
+  }
+}     
+
+
+
+
+
+
+      
+    
+      
