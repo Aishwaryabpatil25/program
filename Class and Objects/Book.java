b248859b15ee5a8ab62e2314ae7259acc1250252
Aishwaryabpatil25 <aishwaryabpatil.xworkz@gmail.com>
2024-01-08 11:59:37 +0530

    class & object in java

--- Class and Objects/Book.java
+++ Class and Objects/Book.java
@@ -10,6 +10,9 @@ public class Book
 
     Book( String title,String author,int publicationYear,int numberOfPages, String type,int price)
        {
+         this.title=title;
+         this.author = author;
+         this.publicationYear = publicationYear;
          this.numberOfPages = numberOfPages;
          this.type = type;
          this.price = price;
@@ -21,6 +24,7 @@ public class Book
             System.out.println("author:"+author);
             System.out.println("publicationYear"+publicationYear);
             System.out.println("numberOfPages:"+numberOfPages);
+            System.out.println("type"+type); 
             System.out.println("price"+price); 
 
          }
--- Class and Objects/BookStore.java
+++ Class and Objects/BookStore.java
@@ -2,7 +2,7 @@ public class BookStore
   {
    public static void main(String args[])
      {
-      Book obj = new Book("three thousand","sudha murthy",128,"novel",150);
+      Book obj = new Book("three thousand","sudha murthy",2017,128,"novel",150);
        obj.display();
      Book.open();
      Book.close();
